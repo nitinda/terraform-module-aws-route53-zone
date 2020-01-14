9a373b756c181b92905d8f8943a22fd333584d6e
nitin-das <nitin.das@thomson.co.uk>
2020-01-14 13:38:37 +0000

    Modified code

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "zone_id" {
+  value = aws_route53_zone.route53_zone.zone_id
+}
+
+output "name_servers" {
+  value = aws_route53_zone.route53_zone.name_servers
+}
---
+++ route53-zone.tf
@@ -0,0 +1,13 @@
+resource "aws_route53_zone" "route53_zone" {
+  name          = var.name
+  comment       = var.comment
+  force_destroy = var.force_destroy
+  tags          = var.tags
+  dynamic "vpc" {
+    for_each = var.vpc
+    content {
+      vpc_id     = vpc.value.vpc_id
+      vpc_region = lookup(vpc.value, "vpc_region", null)
+    }
+  }
+}
